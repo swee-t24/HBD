3a2edf0dc7fd381a5c1b3e866d0464bc9acf4781
swee-t24 <ruhamaalemu31@gmail.com>
2025-04-11 07:51:34 +0300

    Initial commit

---
+++ HBD.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    alert(
+      "Happy Birthday to the only person who can debug life as well as code!\n" +
+      "Hope your day is better than a perfectly running 'Hello World!'\n" +
+      "Thanks for all the guidance and inspiration!"
+    );
+  };
+  
